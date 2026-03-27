a06d7bd226799404d4f29f6353fe87f3c4583905
yjia007126 <yjia007@126.com>
2026-03-27 15:47:13 +0800

    修复：将 ComfyUI 作为普通目录添加

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,15 @@ FROM python:3.10-slim
 
 WORKDIR /
 
+# 1. 先将本地的 ComfyUI 目录拷贝到镜像的 /ComfyUI 路径下
+# 假设你的项目结构是：项目根目录/ComfyUI/requirements.txt
+COPY ComfyUI /ComfyUI
+
 # Install dependencies
 RUN pip install --no-cache-dir runpod
+# RUN pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu130
+# RUN pip install -r ComfyUI/requirements.txt 
+# RUN pip install -U "comfy-script[default]"
 
 # Copy your handler file
 COPY handler.py /
--- handler.py
+++ handler.py
@@ -1,5 +1,11 @@
 import runpod
-import time  
+import time
+
+# from comfy_script.runtime import *
+# # ComfyUI server/path
+# load(r'ComfyUI')  
+# from comfy_script.runtime.nodes import *
+# from pathlib import Path
 
 def handler(event):
 #   This function processes incoming requests to your Serverless endpoint.
@@ -19,11 +25,54 @@ def handler(event):
 
     print(f"Received prompt is hahaha: {prompt}")
     print(f"Sleeping for {seconds} seconds...")
+
+    # output_path = Path('images/sc2.png')
+
+    # with Workflow(wait=True):
+    #   clip = CLIPLoader(clip_name='qwen_3_4b.safetensors', type='lumina2', device='default')
+
+    #   model = UNETLoader('z_image_turbo_bf16.safetensors', 'default')
+    #   model = ModelSamplingAuraFlow(model, 3)
+
+    #   vae = VAELoader('ae.safetensors')
+    
+
+    #   conditioning = CLIPTextEncode('''a girl kiss a cat''', clip)
+    #   conditioning2 = ConditioningZeroOut(conditioning)
+    #   latent = EmptySD3LatentImage(width=544, height=960, batch_size=4)
+    #   latent = KSampler(model=model, seed=611015986142011, steps=9, cfg=1, sampler_name='res_multistep', scheduler='simple', positive=conditioning, negative=conditioning2, latent_image=latent, denoise=1)
+   
+    #   image = VAEDecode(latent, vae)
+    #   images = util.get_images(image)
+    
+    # output_path.parent.mkdir(parents=True, exist_ok=True)
+    # if len(images) == 1:
+    #   images[0].save(output_path)
+    # else:
+    #   for index, generated_image in enumerate(images, start=1):
+    #       numbered_path = output_path.with_name(f'{output_path.stem}_{index:03d}{output_path.suffix}')
+    #       generated_image.save(numbered_path)
+
+
     
     # You can replace this sleep call with your own Python code
-    time.sleep(seconds)  
+    # time.sleep(seconds)  
     
-    return "hi, yjia007, you prompt is : "+prompt 
+    # file_path = r'D:/workspace/PythonProjects/runpod_serverless/models/diffusion_models/cmd.txt'
+    file_path = r'/runpod-volume/models/diffusion_models/cmd.txt'
+
+    try:
+    # 使用 with 语句打开文件，它会在操作完成后自动关闭文件
+    # encoding='utf-8' 参数用于正确读取包含中文等字符的文件，防止乱码
+      with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()  # 读取文件的全部内容
+        # print(content)         # 打印文件内容
+    except FileNotFoundError:
+      content = "错误：找不到文件，请检查路径 '{file_path}' 是否正确。"
+    except UnicodeDecodeError:
+      content = "错误：文件编码错误，请尝试使用其他编码（如 'gbk'）。"
+
+    return "hi, yjia007, you prompt is : " + content 
 
 # Start the Serverless function when the script is run
 if __name__ == '__main__':
